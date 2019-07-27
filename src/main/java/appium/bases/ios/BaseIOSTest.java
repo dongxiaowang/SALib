bf7c71b401ee5b3fa93379817af73ee64df3d1ea
peter <wycgpeterhu@sina.com>
2019-07-27 17:33:16 +0800

    INIT PTOJECT

---
+++ src/main/java/appium/bases/ios/BaseIOSTest.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package appium.bases.ios;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.junit.AfterClass;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class BaseIOSTest {
+
+    protected static AppiumDriverLocalService service;
+    protected static IOSDriver<IOSElement> driver;
+    protected static final int PORT = 4723;
+    public static final String DEVICE_NAME = System.getenv("IOS_DEVICE_NAME") != null
+            ? System.getenv("IOS_DEVICE_NAME") : "iPhone X";
+    public static final String PLATFORM_VERSION = System.getenv("IOS_PLATFORM_VERSION") != null
+            ? System.getenv("IOS_PLATFORM_VERSION") : "12.4";
+
+
+    /**
+     * Starts a local server.
+     *
+     * @return ip of a local host
+     * @throws UnknownHostException when it is impossible to get ip address of a local host
+     */
+    public static String startAppiumServer() throws UnknownHostException {
+        service = new AppiumServiceBuilder()
+                .usingPort(PORT).build();
+        service.start();
+        InetAddress inetAddress = InetAddress.getLocalHost();
+//        return inetAddress.getHostAddress();
+        return "192.168.0.101";
+    }
+
+
+    /**
+     * finishing.
+     */
+    @AfterClass public static void afterClass() {
+        if (driver != null) {
+            driver.quit();
+        }
+        if (service != null) {
+            service.stop();
+        }
+    }
+}
---
+++ src/main/java/appium/bases/ios/BaseSafariTest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package appium.bases.ios;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileBrowserType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumServerHasNotBeenStartedLocallyException;
+import org.junit.BeforeClass;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+public class BaseSafariTest extends BaseIOSTest {
+
+    @BeforeClass public static void beforeClass() throws UnknownHostException, MalformedURLException {
+        final String ip = startAppiumServer();
+
+        if (service == null || !service.isRunning()) {
+            throw new AppiumServerHasNotBeenStartedLocallyException("An appium server node is not started!");
+        }
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, MobileBrowserType.SAFARI);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, PLATFORM_VERSION);
+        //sometimes environment has performance problems
+        capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 500000);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, DEVICE_NAME);
+        driver = new IOSDriver<>(new URL("http://" + ip + ":" + PORT + "/wd/hub"), capabilities);
+    }
+}
